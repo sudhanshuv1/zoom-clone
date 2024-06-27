72d582a9158a91f09cdfa16e0168abbfc38605d6
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2024-06-27 21:20:35 +0530

    Create the homepage section

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
 import Image from 'next/image'
+import { SignedIn } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -18,7 +19,9 @@ const Navbar = () => {
       </Link>
 
       <div className="flex-between gap-5">
-        {/*Clerk user management*/}
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
         <MobileNav />
       </div>
     </nav>
