c7d67e88861c575cd442746c19f01b66e20b9894
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2024-08-30 14:44:43 +0530

    Final commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,33 @@
-import Link from 'next/link'
-import React from 'react'
-import MobileNav from './MobileNav'
-import Image from 'next/image'
-import { SignedIn } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
+import { SignedIn, UserButton } from '@clerk/nextjs';
+
+import MobileNav from './MobileNav';
 
 const Navbar = () => {
   return (
-    <nav className='flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10'>
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
           width={32}
           height={32}
-          alt = "Yoom logo"
-          className = "max-sm:size-10"
+          alt="yoom logo"
+          className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yoom</p>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          YOOM
+        </p>
       </Link>
-
       <div className="flex-between gap-5">
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
+        <SignedIn>
+          <UserButton afterSignOutUrl="/sign-in" />
+        </SignedIn>
+
         <MobileNav />
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- constants/index.ts
+++ constants/index.ts
@@ -1,27 +1,36 @@
 export const sidebarLinks = [
-    {
-        label: 'Home',
-        route: '/',
-        imgUrl: '/icons/Home.svg',
-    },
-    {
-        label: 'Upcoming',
-        route: '/upcoming',
-        imgUrl: '/icons/upcoming.svg',
-    },
-    {
-        label: 'Previous',
-        route: '/previous',
-        imgUrl: '/icons/previous.svg',
-    },
-    {
-        label: 'Recordings',
-        route: '/recordings',
-        imgUrl: '/icons/video.svg',
-    },
-    {
-        label: 'Personal Room',
-        route: '/personal-room',
-        imgUrl: '/icons/add-personal.svg',
-    }
-]
+  {
+    imgURL: '/icons/Home.svg',
+    route: '/',
+    label: 'Home',
+  },
+
+  {
+    imgURL: '/icons/upcoming.svg',
+    route: '/upcoming',
+    label: 'Upcoming',
+  },
+  {
+    imgURL: '/icons/previous.svg',
+    route: '/previous',
+    label: 'Previous',
+  },
+  {
+    imgURL: '/icons/Video.svg',
+    route: '/recordings',
+    label: 'Recordings',
+  },
+  {
+    imgURL: '/icons/add-personal.svg',
+    route: '/personal-room',
+    label: 'Personal Room',
+  },
+];
+
+export const avatarImages = [
+  '/images/avatar-1.jpeg',
+  '/images/avatar-2.jpeg',
+  '/images/avatar-3.png',
+  '/images/avatar-4.png',
+  '/images/avatar-5.png',
+];
