12f435f0c66181cab2cba7db1fcc3e3724bdb8ee
Sudhanshu Tiwari <sudhanshu.98t@gmail.com>
2024-11-11 22:09:51 +0530

    Update meeting link

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -53,7 +53,7 @@ const PersonalRoom = () => {
     router.push(`/meeting/${meetingId}?personal=true`);
   };
 
-  const meetingLink = `http://zoom-clone-stream.vercel.app/meeting/${meetingId}?personal=true`;
+  const meetingLink = `https://zoom-clone-stream.vercel.app/meeting/${meetingId}?personal=true`;
 
   return (
     <section className="flex size-full flex-col gap-10 text-white">
